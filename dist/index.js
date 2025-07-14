46d226a90c961da321bece1f8c505c6cae3ce11b
Rene Borigas <rinborigas@gmail.com>
2025-07-15 06:51:38 +0800

    learner-dash

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 import Header from './Header';
 import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
-import StudioHeader from './studio-header';
-import LearnerDashboardHeader from 'LearnerDashboardHeader';
-export { LearningHeader, messages, StudioHeader,LearnerDashboardHeader };
+import StudioHeader from './studio-header'; 
+export { LearningHeader, messages, StudioHeader };
+export { default as LearnerDashboardHeader } from './LearnerDashboardHeader';
 export default Header;
 //# sourceMappingURL=index.js.map
 
