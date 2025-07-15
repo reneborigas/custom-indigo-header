c5730a8f2353bf7ca5e8fee64f994911efca4c68
Rene Borigas <rinborigas@gmail.com>
2025-07-15 08:12:26 +0800

    revert

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,7 @@ import Header from './Header';
 import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
 import StudioHeader from './studio-header';
-import LearnerDashboardHeader from 'LearnerDashboardHeader';
-export { LearningHeader, messages, StudioHeader,LearnerDashboardHeader };
+export { LearningHeader, messages, StudioHeader,Header };
 export default Header;
 //# sourceMappingURL=index.js.map
 
