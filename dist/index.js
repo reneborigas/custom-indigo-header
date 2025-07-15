ae0318292d347a8d2443ab5314bbda49597045a9
Rene Borigas <rinborigas@gmail.com>
2025-07-15 08:23:50 +0800

    v1

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,11 @@ import Header from './Header';
 import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
 import StudioHeader from './studio-header';
-export { LearningHeader, messages, StudioHeader,Header };
-export default Header;
-//# sourceMappingURL=index.js.map
 
-// import './test.scss';
+// 👇 Add this to inspect
+console.log('Header value:', Header);
+
+export { LearningHeader, messages, StudioHeader, Header };
+export default Header;
+
+//# sourceMappingURL=index.js.map
